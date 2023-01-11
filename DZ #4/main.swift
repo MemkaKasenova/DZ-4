0350a41ce2e3c56bd801ceb3ef5f0655879ca42f
Memka <mkasenova87@gmail.com>
2023-01-11 18:11:12 +0600

    ДОПОЛНЕНИЕ

--- DZ #4/main.swift
+++ DZ #4/main.swift
@@ -11,49 +11,36 @@ import Foundation
 //Задание 1) Создать массив из имен машин.
 
 var autos: [String] = ["MERSEDES", "AUDI" , "MAZDA", "LEXUS", "BMW","PORSHE"]
+    print(autos)
 
 //Задание 2) Создать массив из курсов валют типа Float.
 //
 //Создать переменную купюра с номиналом 1000 сом. Сделать так чтобы 1000 сом отобразилось в разных валютах: доллары, евро, тенге и т.д. с помощью цикла.
 
-let KZG: [Float] = [1000.00]
-let USD: [Float] = [86.68]
-let EUR: [Float] = [95.41]
-let KZT: [Float] = [0.1999]
+var KGZ = 1000.00
+let USD: Float = 86.68
+let EUR: Float = 95.41
+let KZT: Float = 0.1999
 
-let valuta = [KZG,USD,EUR,KZT]
+let valuta = [USD,EUR,KZT]
 
-for index in  {
-    if itemUSD == USD * KZG {
-        print(itemUSD)
-    }else if iEUR == EUR * KZG{
-        print(iEUR)
-    }else if iKZT == KZT * KZG {
-        print(iKZG)
-    }
-    
-//Задание 3) Создать массив имен учитывая алфавит. Пройтись циклом и считать сколько имен имеется на букву “A” и т.д.
-
-var names: [String] = ["Atake","Adil", "Daian", "Iskender", "Kanykei"]
-
-    if names == "A"{
-        print("Сколько имен на букву А?")
-    }else{
-        print("Нет имен с буквой А")
-    }
-print("В массиве имен на букву А:")
-
-var searchName = true
-    
-for name in names {
-    if name == searchNames{
-        isFound = true
-        break
-    }else{
-        isFound == false
-    }
-    print(item)
+for item in valuta {
+    print(Float(KGZ) * item)
 }
-
+    
+    //Задание 3) Создать массив имен учитывая алфавит. Пройтись циклом и считать сколько имен имеется на букву “A” и т.д.
+    
+var names: [String] = ["Atake","Adil", "Daian", "Iskender", "Kanykei"]
+let alphabet = ["A"]
+for letter in alphabet{
+    var count = 0
+    for name in names {
+        if name.first! ==
+            Character(letter) {
+            count += 1
+        }
+    }
+    print("Количество имен на букву \(letter):\(count)")
+}
 
 
